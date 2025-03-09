5684b16dfd18d0b3cca6091e159fa2f853c7140e
samuel <ibenjaminsamuel47@gmail.com>
2025-03-09 14:25:05 +0100

    last commit

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-const btn = document.querySelector('.button');
-const body= document.querySelector(".body");
-const paragraph = document.getElementsByTagName("p")
+const btn = document.querySelector("button");
+const body = document.querySelector('body');
 btn.addEventListener("click",onclick);
 function onclick(e){
-body.classList.add('john');
-// paragraph.style
-btn.classList.add("james");
-btn.innerHTML="click to change to light mode"
+if(btn.value===''){
+    body.style.background='black'
+    body.style.color='white'
+    btn.style.background='white'
+    btn.style.color='black';
+    btn.value='hi'
+    // console.log('hello')
+    btn.innerHTML="click to change to dark mode"
+}else{
+    btn.innerHTML='click to change to light mode'
+    body.style.background="white"
+    body.style.color='black'
+    btn.style.background="black"
+    btn.style.color='white'
+    btn.value=""
+    // console.log("hi");
+    
 }
-
+}
